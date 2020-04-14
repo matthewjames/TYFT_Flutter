4bb0546bc8fb432818d7b795ef4e0b2c8e810a12
Matt James <matt.james1310@gmail.com>
2020-04-13 23:10:31 -0700

    DashboardPage: Fixed Chart refresh of new data

--- lib/page_chart.dart
+++ lib/page_chart.dart
@@ -1,27 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class ChartPage extends StatefulWidget {
-  ChartPage({Key key, this.title, this.chart}) : super(key: key);
+  ChartPage({Key key, this.title}) : super(key: key);
 
   static const String routeName = "/ChartPage";
 
   final String title;
-  Widget chart;
 
   @override
   _ChartPageState createState() => new _ChartPageState();
 }
 
-/// // 1. After the page has been created, register it with the app routes
-/// routes: <String, WidgetBuilder>{
-///   ChartPage.routeName: (BuildContext context) => new ChartPage(title: "ChartPage"),
-/// },
-///
-/// // 2. Then this could be used to navigate to the page.
-/// Navigator.pushNamed(context, ChartPage.routeName);
-///
-
 class _ChartPageState extends State<ChartPage> {
+  static DatabaseReference _firebaseRef = FirebaseDatabase.instance.reference();
+  Widget chart = FutureBuilder(
+      future: _firebaseRef.child("temp2/uid/restaurant/shifts/").once(),
+      builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+        List<Shift> data = new List<Shift>();
+        if (snapshot.hasData) {
+          data.clear();
+          Map<dynamic, dynamic> values = snapshot.data.value;
+          values.forEach((key, values) {
+            int tipAmount = int.parse(values['shift_data']['tips']['take_home']);
+            print("Date: " + key + " Tip Amount: " + tipAmount.toString());
+            DateTime date = DateTime.fromMillisecondsSinceEpoch(int.parse(key));
+
+            data.add(new Shift(date, tipAmount));
+          });
+          data.sort((a,b) => a.date.compareTo(b.date));
+
+          print(data);
+
+          return Flex(
+            children: <Widget>[
+              Expanded(child: new SimpleTimeSeriesChart.withShiftData(data))
+            ], direction: Axis.horizontal,
+          );
+        }
+        return Flex(
+          children: <Widget>[
+            Expanded(child: Center(child: new CircularProgressIndicator()))
+          ], direction: Axis.horizontal,
+        );
+      });
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +69,7 @@ class _ChartPageState extends State<ChartPage> {
                           Expanded(
                             flex: 8,
                             child: Container(
-                                child: widget.chart
+                                child: chart
                             ),
                           ),
                           Expanded(
@@ -64,4 +87,52 @@ class _ChartPageState extends State<ChartPage> {
         )
     );
   }
+}
+
+class SimpleTimeSeriesChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+
+  SimpleTimeSeriesChart(this.seriesList);
+
+  factory SimpleTimeSeriesChart.withShiftData(List<Shift> shifts) {
+    List<TimeSeriesSimple> mapped = shifts.map((item) {
+      return TimeSeriesSimple(item.date, item.tip_amt);
+    }).toList();
+
+    List<charts.Series<TimeSeriesSimple, DateTime>> list = [
+      charts.Series<TimeSeriesSimple, DateTime>(
+        id: 'Tips',
+        colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
+        domainFn: (TimeSeriesSimple shift, _) => shift.date,
+        measureFn: (TimeSeriesSimple shift, _) => shift.tips,
+        data: mapped,
+      )
+    ];
+
+    return SimpleTimeSeriesChart(list);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return charts.TimeSeriesChart(
+      seriesList,
+      animationDuration: Duration(milliseconds: 500),
+      dateTimeFactory: const charts.LocalDateTimeFactory(),
+    );
+  }
+}
+
+/// Sample time series data type.
+class TimeSeriesSimple {
+  final DateTime date;
+  final int tips;
+
+  TimeSeriesSimple(this.date, this.tips);
+}
+
+class Shift {
+  final DateTime date;
+  final int tip_amt;
+
+  Shift(this.date, this.tip_amt);
 }
--- lib/page_dashboard.dart
+++ lib/page_dashboard.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:firebase_database/firebase_database.dart';
 import 'package:tyft_f/page_chart.dart';
 import 'package:page_view_indicators/page_view_indicators.dart';
@@ -14,42 +13,13 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
-  static DatabaseReference _firebaseRef = FirebaseDatabase.instance.reference();
-  static Widget chart = FutureBuilder(
-      future: _firebaseRef.child("temp2/uid/restaurant/shifts/").once(),
-      builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
-        List<Shift> data = new List<Shift>();
-        if (snapshot.hasData) {
-          data.clear();
-          Map<dynamic, dynamic> values = snapshot.data.value;
-          values.forEach((key, values) {
-            int tipAmount = int.parse(values['shift_data']['tips']['take_home']);
-            print("Date: " + key + " Tip Amount: " + tipAmount.toString());
-            DateTime date = DateTime.fromMillisecondsSinceEpoch(int.parse(key));
 
-            data.add(new Shift(date, tipAmount));
-          });
-          data.sort((a,b) => a.date.compareTo(b.date));
 
-          print(data);
-
-          return Flex(
-              children: <Widget>[
-                Expanded(child: new SimpleTimeSeriesChart.withShiftData(data))
-              ], direction: Axis.horizontal,
-          );
-        }
-        return Flex(
-            children: <Widget>[
-              Expanded(child: Center(child: new CircularProgressIndicator()))
-            ], direction: Axis.horizontal,
-        );
-      });
 
   final List<Widget> _charts = [
-    new ChartPage(title: "Chart 1", chart: chart),
-    new ChartPage(title: "Chart 2", chart: chart),
-    new ChartPage(title: "Chart 3", chart: chart)
+    new ChartPage(title: "Chart 1"),
+    new ChartPage(title: "Chart 2"),
+    new ChartPage(title: "Chart 3")
   ];
 
   final _pageController = PageController(
@@ -119,50 +89,4 @@ class _DashboardPageState extends State<DashboardPage> {
 
 
 
-class SimpleTimeSeriesChart extends StatelessWidget {
-  final List<charts.Series> seriesList;
 
-  SimpleTimeSeriesChart(this.seriesList);
-
-  factory SimpleTimeSeriesChart.withShiftData(List<Shift> shifts) {
-    List<TimeSeriesSimple> mapped = shifts.map((item) {
-      return TimeSeriesSimple(item.date, item.tip_amt);
-    }).toList();
-
-    List<charts.Series<TimeSeriesSimple, DateTime>> list = [
-      charts.Series<TimeSeriesSimple, DateTime>(
-        id: 'Tips',
-        colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
-        domainFn: (TimeSeriesSimple shift, _) => shift.date,
-        measureFn: (TimeSeriesSimple shift, _) => shift.tips,
-        data: mapped,
-      )
-    ];
-
-    return SimpleTimeSeriesChart(list);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return charts.TimeSeriesChart(
-      seriesList,
-      animationDuration: Duration(milliseconds: 500),
-      dateTimeFactory: const charts.LocalDateTimeFactory(),
-    );
-  }
-}
-
-/// Sample time series data type.
-class TimeSeriesSimple {
-  final DateTime date;
-  final int tips;
-
-  TimeSeriesSimple(this.date, this.tips);
-}
-
-class Shift {
-  final DateTime date;
-  final int tip_amt;
-
-  Shift(this.date, this.tip_amt);
-}
